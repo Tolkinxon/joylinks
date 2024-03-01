1410412002d1af1fa02d60a8e785157fe5d08e4f
Tolkinxon <krosstony639@gmail.com>
2024-03-01 13:22:41 +0300

    firth commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react'
-import './App.css'
+import { React, useState, useEffect } from 'react'
+
+
 
 export default function App() {
-  return <div className="container"></div>
+
+  useEffect(() => {
+    const host = 'api.frankfurter.app';
+    
+  fetch(`https://${host}/latest?amount=10&from=INR&to=USD`)
+    .then(resp => resp.json())
+    .then((data) => {
+      alert(`10 EUR = ${data.rates.USD} USD`);
+    });
+  })
+
+
+  return (
+    <>
+    
+    </>
+  )
 }
